9c82a6cdd2e43da9435d3c1df65fcd775a03f960
Ken Burkhardt <ken.burkhardt@gmail.com>
2011-10-03 09:17:00 -0400

    initial

---
+++ HelloWorld/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using log4net;
+using NServiceBus;
+
+namespace HelloWorld 
+{
+    public class Class1 : IConfigureThisEndpoint, IWantToRunAtStartup
+    {
+        public void Run()
+        {
+            LogManager.GetLogger("Class1").Info("Hello world!");
+        }
+
+        public void Stop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
